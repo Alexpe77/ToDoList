776f63916db07057e1dc8382e861f70604bf7bfe
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-04-25 16:55:07 +0200

    First commit

---
+++ assets/js/collection.js
@@ -0,0 +1,25 @@
+const mytodoList = [
+
+{
+    task: "Do the dishes",
+},
+{
+    task: "Go to the grocery store",
+},
+{
+    task: "Clean the house",
+},
+{
+    task: "Go to the gym",
+},
+{
+    task: "Sleep",
+},
+{
+    task: "Walk the dog",
+},
+{
+    task: "Doctor appointment",
+},
+
+]
---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const taskInput = document.querySelector(".taskInput");
+const submitBtn = document.querySelector(".submitBtn");
+const toDoList = document.querySelector(".toDoList");
+const todoSelection = document.querySelector(".todoSelection");
+
+document.addEventListener()
